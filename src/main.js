2c7899c20ebe36027ef6b0cacbb3252d4313c5c5
KhoaNguyen <78020093+khoadeu25890@users.noreply.github.com>
2022-09-22 18:54:43 +0700

    WEB-24 help in config redux

---
+++ src/main.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App'
+import './index.css'
+import { store } from './app/store.js'
+import { BrowserRouter } from 'react-router-dom'
+import { Provider } from 'react-redux'
+
+ReactDOM.render(
+  <Provider store={store} >
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+   </Provider>,
+   document.getElementById('root') 
+)
---
+++ src/slice/ProductSlice.js
@@ -0,0 +1,9 @@
+import { createSlice, createAsyncThunk } from "@reduxjs/toolkit"
+
+export const productSlice = createSlice({
+    name: 'product',
+    initialState,
+    reducers:{}
+})
+
+export default productSlice.reducer
